2ae5c9a46ff70d39cae9543aebb552ff13344653
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-04-20 20:51:23 -0500

    clarifying readableDate filter

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
+  // this is kinda hacky...? no guarantee the host computer that i create commits on is CST...
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "America/Chicago" }).toFormat("MMMM d, yyyy");
   });
