fd3417ab09a3ad703be43c625a9d3aba62c6c359
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-04-20 20:48:32 -0500

    adjusted config to account for github runner timezones?

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat("MMMM d, yyyy");
+    return DateTime.fromJSDate(dateObj, { zone: "America/Chicago" }).toFormat("MMMM d, yyyy");
   });
 
   return {
