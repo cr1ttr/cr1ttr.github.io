ff447f4e4f3607645dfd9d67fff97f2196c14ce6
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-04-29 05:26:41 -0500

    initial commit

---
+++ src/data/navbar_links.ts
@@ -0,0 +1,10 @@
+import type Link from "../types/link"
+
+const NAVBAR_LINKS: Link[] = [
+  { label: "HOME", href: "/" },
+  { label: "ABOUT", href: "/about" },
+  { label: "PROJECTS", href: "/projects" },
+  { label: "BLOG", href: "/blog" }
+]
+
+export default NAVBAR_LINKS;
