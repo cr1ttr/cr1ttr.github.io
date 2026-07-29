60ad5e4daf2c1ecef94affb0e8d36c5a30a857db
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-07-29 08:09:48 -0500

    idk man one day ill have good commit messages

--- src/data/88x31_data.ts
+++ src/data/88x31_data.ts
@@ -1,11 +1,11 @@
 import type ImageAnchor from "../types/image_anchor"
 
 const IMAGE_ANCHORS: ImageAnchor[] = [
-  { href: "https://cr1ttr.github.io/", src: "/cr1ttr-88x31.gif" },
-  { href: "https://null-version-nova.github.io/", src: "/manynamedangel.gif" },
-  { href: "https://sugrstrz.com/", src: "/SugrBadge.png" },
-  { href: "https://mint.pages.gay/", src: "/mint-88x31.gif" },
-  { href: "https://zoey-on-github.github.io/", src: "/julie-88x31.png" }
+  { href: "https://null-version-nova.github.io/", src: "/badges/manynamedangel-88x31.gif" },
+  { href: "https://sugrstrz.com/", src: "/badges/sugrstrz-88x31.png" },
+  { href: "https://mint.pages.gay/", src: "/badges/mint-88x31.gif" },
+  { href: "https://zoey-on-github.github.io/", src: "/badges/julie-88x31.png" },
+  { href: "https://aivi.party/", src: "/badges/aivi-party-88x31-2.gif" }
 ];
 
 export default IMAGE_ANCHORS;
---
+++ src/data/index_supplements.ts
@@ -0,0 +1,12 @@
+
+// please for the love of god automate this at some point
+export const CURRENTLY_PLAYING_KEY_ART = "https://cdn.akamai.steamstatic.com/steam/apps/3195790/header.jpg"
+export const CURRENTLY_PLAYING_NAME = "White Knuckle"
+export const CURRENTLY_PLAYING_DEVELOPER = "Dark Machine Games"
+export const CURRENTLY_PLAYING_PUBLISHER = "DreadXP"
+export const CURRENTLY_PLAYING_REDIRECT = "https://store.steampowered.com/app/3195790/White_Knuckle/"
+
+export const CURRENTLY_LISTENING_KEY_ART = "https://i.scdn.co/image/ab67616d0000e1a32aa7e9feb1a56026a8c212ae"
+export const CURRENTLY_LISTENING_NAME = "Seven Dollars"
+export const CURRENTLY_LISTENING_ARTIST = "Happy Birthday Mr. Baskets"
+export const CURRENTLY_LISTENING_REDIRECT = "https://www.youtube.com/watch?v=ldKoXzVdMGw"
